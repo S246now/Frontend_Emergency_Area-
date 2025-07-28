f1a889ab30e841c4733d5f7ae7fa53f52fbd096c
S246now <odalissearguellootmail.com>
2025-07-27 20:16:36 -0500

    feat: add css for menu and navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import MainNavbar from "@/components/MainNavbar";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MainNavbar/>
+        <Navbar/>
         {children}
       </body>
     </html>
